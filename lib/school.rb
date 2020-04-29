2eba92866f00bed4d3935668354b8f8b25acad5f
Jared  E <jared.epps322@gmail.com>
2020-04-29 19:18:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,3 +5,8 @@ school.roster
 
 hash["new_key"] << "new_value_for_value_array"
 
+hash["new_key"] = []
+hash["new_key"] << "new_value_for_value_array"
+ 
+hash
+ => {"new_key"=>["new_value_for_value_array"]} 
